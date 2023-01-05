a2424a4e231b67ddfc932d4b4abc4541709f1602
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:22:47 +0900

    Init: Create 중앙값 구하기.cpp

---
+++ Programmers/Lv. 0/중앙값 구하기.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 중앙값 구하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230105
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    sort(array.begin(), array.end());
+    answer = array[array.size() / 2];
+    return answer;
+}
