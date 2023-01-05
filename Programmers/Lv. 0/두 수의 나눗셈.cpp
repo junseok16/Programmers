2ee6a1164f4420ad65895ca774309612678dc6a0
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:20:33 +0900

    Init: Create 두 수의 나눗셈.cpp

---
+++ Programmers/Lv. 0/두 수의 나눗셈.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* @title    : 두 수의 나눗셈(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230104
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num1, int num2) {
+    double answer = (double)num1 / num2 * 1000;
+    return (int)answer;
+}
