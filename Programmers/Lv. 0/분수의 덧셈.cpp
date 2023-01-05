fbc1312e0d3d1f247a2e82a930107073da79e9e9
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:22:30 +0900

    Init: Create 분수의 덧셈.cpp

---
+++ Programmers/Lv. 0/분수의 덧셈.cpp
@@ -0,0 +1,23 @@
+﻿/*
+* @title    : 분수의 덧셈(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230104
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+vector<int> solution(int denum1, int num1, int denum2, int num2) {
+    vector<int> answer;
+    int a = denum1 * num2 + num1 * denum2;
+    int b = num1 * num2;
+    int gcd = std::gcd(a, b);
+
+    answer.push_back(a / gcd);
+    answer.push_back(b / gcd);
+    return answer;
+}
