da7ebf308fed727eb3616c23d871c9edaa998724
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:20:52 +0900

    Init: Create 두 수의 합.cpp

---
+++ Programmers/Lv. 0/두 수의 합.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* @title    : 두 수의 합(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date	    : 230103
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    answer = num1 + num2;
+    return answer;
+}
