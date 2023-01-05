f56b436c742006490882f9f602e04c8e80479b2b
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:21:01 +0900

    Init: Create 몫 구하기.cpp

---
+++ Programmers/Lv. 0/몫 구하기.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* @title    : 몫 구하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = num1 / num2;
+    return answer;
+}
