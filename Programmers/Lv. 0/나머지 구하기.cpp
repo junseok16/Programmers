f89737f403f77bcec5e9c0ef043195e64971ca93
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:20:02 +0900

    Init: Create 나머지 구하기.cpp

---
+++ Programmers/Lv. 0/나머지 구하기.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* @title    : 나머지 구하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230105
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 % num2;
+    return answer;
+}
