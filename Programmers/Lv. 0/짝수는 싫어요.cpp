74466454341a0d68d87108961e6594e443709be0
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:22:57 +0900

    Init: Create 짝수는 싫어요.cpp

---
+++ Programmers/Lv. 0/짝수는 싫어요.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 짝수는 싫어요(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230105
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    for (int i = 1; i <= n; i += 2) {
+        answer.push_back(i);
+    }
+    return answer;
+}
