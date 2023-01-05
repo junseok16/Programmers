b5e68753d7258ef0e901be70623c1945d4b2c08d
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:21:26 +0900

    Init: Create 배열 두 배 만들기.cpp

---
+++ Programmers/Lv. 0/배열 두 배 만들기.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 배열 두 배 만들기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for (const auto& e : numbers) {
+        answer.push_back(e * 2);
+    }
+    return answer;
+}
