99404bfc4864960b5c784e04144298f82a357024
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 00:20:22 +0900

    Init: Create 두 수의 곱.cpp

---
+++ Programmers/Lv. 0/두 수의 곱.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* @title    : 두 수의 곱(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    cin >> num1 >> num2;
+    answer = num1 * num2;
+    return answer;
+}
