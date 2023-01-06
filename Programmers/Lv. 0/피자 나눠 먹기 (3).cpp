ba883cd25e6853f42f34a2a615e2a47118e108b4
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 11:23:21 +0900

    Init: Create 피자 나눠 먹기 (3).cpp

---
+++ Programmers/Lv. 0/피자 나눠 먹기 (3).cpp
@@ -0,0 +1,20 @@
+﻿/*
+* @title    : 피자 나눠 먹기 (3)(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230106
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+    answer = n / slice;
+    if (n % slice != 0) {
+        ++answer;
+    }
+    return answer;
+}
