b99fe3202b2f638adaa37f58113df7e7c75bd990
Junseok Tak <tjsuk0220@gmail.com>
2023-01-06 10:28:13 +0900

    Init: Create 배열의 평균값.cpp

---
+++ Programmers/Lv. 0/배열의 평균값.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* @title    : 배열의 평균값(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230106
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    for (const auto& e : numbers) {
+        answer += e;
+    }
+    answer /= numbers.size();
+    return answer;
+}
