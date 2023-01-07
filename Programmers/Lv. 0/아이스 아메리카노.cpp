35a29b779af5fe53c5a4d5a0cca11d810abe3648
Junseok Tak <tjsuk0220@gmail.com>
2023-01-07 10:14:02 +0900

    Init: Create 아이스 아메리카노.cpp

---
+++ Programmers/Lv. 0/아이스 아메리카노.cpp
@@ -0,0 +1,18 @@
+﻿/*
+* @title    : 아이스 아메리카노(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230107
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
+    return answer;
+}
