c5cae2a9068703b8d2aa128d2fb495f939d75081
Junseok Tak <tjsuk0220@gmail.com>
2023-01-07 10:13:44 +0900

    Init: Create 나이 출력.cpp

---
+++ Programmers/Lv. 0/나이 출력.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* @title    : 나이 출력(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230107
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    answer = 2022 - age + 1;
+    return answer;
+}
