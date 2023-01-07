5f30adc91abb1a8926f91bfce4490c7a62af08db
Junseok Tak <tjsuk0220@gmail.com>
2023-01-07 10:13:54 +0900

    Init: Create 배열 뒤집기.cpp

---
+++ Programmers/Lv. 0/배열 뒤집기.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 나이 출력(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230107
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    reverse(num_list.begin(), num_list.end());
+    answer = num_list;
+    return answer;
+}
