9ba90e9146a25cae66b0a408548a7c372bdd9ba1
Junseok Tak <tjsuk0220@gmail.com>
2023-01-08 23:35:13 +0900

    Init: Create 직각삼각형 출력하기.cpp

---
+++ Programmers/Lv. 0/직각삼각형 출력하기.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* @title    : 직각삼각형 출력하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230108
+* @state    : 완료
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i + 1; ++j) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
