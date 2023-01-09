e35ca47855a5bab2fe99c0dc3f0c4fcc2cb1b723
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:11:36 +0900

    Init: Create 특정 문자 제거하기.cpp

---
+++ Programmers/Lv. 0/특정 문자 제거하기.cpp
@@ -0,0 +1,23 @@
+﻿/*
+* @title    : 특정 문자 제거하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230109
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+
+    for (const auto& e : my_string) {
+        if (e != *letter.c_str()) {
+            answer.push_back(e);
+        }
+    }
+
+    return answer;
+}
