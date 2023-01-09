23d8b74a354d546e63196da541a5513bc858de37
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:10:39 +0900

    Init: Create 각도기.cpp

---
+++ Programmers/Lv. 0/각도기.cpp
@@ -0,0 +1,29 @@
+﻿/*
+* @title    : 각도기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230109
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+
+    if (0 < angle && angle < 90) {
+        answer = 1;
+    }
+    else if (angle == 90) {
+        answer = 2;
+    }
+    else if (90 < angle && angle < 180) {
+        answer = 3;
+    }
+    else if (angle == 180) {
+        answer = 4;
+    }
+    return answer;
+}
