3dc038d60f0009455b90975d9d02e2cc49082b2c
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:11:07 +0900

    Init: Create 양꼬치.cpp

---
+++ Programmers/Lv. 0/양꼬치.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 양꼬치(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230109
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n, int k) {
+    int answer = 0;
+    int sheep = n * 12000;
+    int beverage = (k - n / 10) * 2000;
+    answer = sheep + beverage;
+    return answer;
+}
