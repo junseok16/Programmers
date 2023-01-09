cc304997775dba5425c5982453a39236fdb18c6b
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:11:00 +0900

    Init: Create 문자열 뒤집기.cpp

---
+++ Programmers/Lv. 0/문자열 뒤집기.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 문자열 뒤집기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230108
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    reverse(my_string.begin(), my_string.end());
+    answer = my_string;
+    return answer;
+}
