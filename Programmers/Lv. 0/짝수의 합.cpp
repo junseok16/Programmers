78c9dd4b26f9c089c66d209f888d1e6acdcf51e8
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:11:25 +0900

    Init: Create 짝수의 합.cpp

---
+++ Programmers/Lv. 0/짝수의 합.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* @title    : 짝수의 합(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230109
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 2 == 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
