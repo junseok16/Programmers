0be5ab3906a9dae0442466e8542ac29f087085ee
Junseok Tak <tjsuk0220@gmail.com>
2023-01-09 10:11:17 +0900

    Init: Create 짝수 홀수 개수.cpp

---
+++ Programmers/Lv. 0/짝수 홀수 개수.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* @title    : 짝수 홀수 개수(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230108
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer(2, 0);
+    for (const auto& e : num_list) {
+        if (e % 2 == 0) { ++answer[0]; }
+        else { ++answer[1]; }
+    }
+    return answer;
+}
