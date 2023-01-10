b988b358758d61753a643c6fd352caf64ce9ff3f
Junseok Tak <tjsuk0220@gmail.com>
2023-01-10 18:35:26 +0900

    Init: Create 진료 순서 정하기.cpp

---
+++ Programmers/Lv. 0/진료 순서 정하기.cpp
@@ -0,0 +1,27 @@
+﻿/*
+* @title    : 진료 순서 정하기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230110
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> emergency) {
+    vector<int> answer, sorted;
+    sorted = emergency;
+    sort(sorted.begin(), sorted.end(), greater<>());
+
+    for (unsigned int i = 0; i < emergency.size(); ++i) {
+        for (unsigned int j = 0; j < sorted.size(); ++j) {
+            if (emergency[i] == sorted[j]) {
+                answer.push_back(j + 1);
+            }
+        }
+    }
+    return answer;
+}
