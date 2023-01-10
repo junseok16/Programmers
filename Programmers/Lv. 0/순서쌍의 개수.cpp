dec72ed4d28aad405ff2c14eb24dc6623a0d2af6
Junseok Tak <tjsuk0220@gmail.com>
2023-01-10 18:35:54 +0900

    Init: Create 순서쌍의 개수.cpp

---
+++ Programmers/Lv. 0/순서쌍의 개수.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* @title    : 순서쌍의 개수(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230110
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            ++answer;
+        }
+    }
+    return answer;
+}
