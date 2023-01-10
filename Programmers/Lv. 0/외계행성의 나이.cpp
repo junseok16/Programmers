5706dd3d32049b02ef3340299eb681107bbfb31f
Junseok Tak <tjsuk0220@gmail.com>
2023-01-10 18:35:40 +0900

    Init: Create 외계행성의 나이.cpp

---
+++ Programmers/Lv. 0/외계행성의 나이.cpp
@@ -0,0 +1,24 @@
+﻿/*
+* @title    : 외계행성의 나이(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230110
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(int age) {
+    string answer = "";
+
+    while (age > 0) {
+        char digit = age % 10 + 97;
+        answer.push_back(digit);
+        age /= 10;
+    }
+    reverse(answer.begin(), answer.end());
+    return answer;
+}
