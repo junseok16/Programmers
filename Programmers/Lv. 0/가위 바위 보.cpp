55dd8c9a66d981ae4a9c8c0a829f58f3ddb85184
Junseok Tak <tjsuk0220@gmail.com>
2023-01-11 19:25:13 +0900

    Init: Create 가위 바위 보.cpp

---
+++ Programmers/Lv. 0/가위 바위 보.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* @title    : 가위 바위 보(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230111
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for (const auto& e : rsp) {
+        if (e == '2') { answer.push_back('0'); }
+        else if (e == '0') { answer.push_back('5'); }
+        else if (e == '5') { answer.push_back('2'); }
+    }
+    return answer;
+}
