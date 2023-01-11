74dc47bd43fd8d05a8979152a5e5687a192a166d
Junseok Tak <tjsuk0220@gmail.com>
2023-01-11 19:24:51 +0900

    Init: Create 모스부호 (1).cpp

---
+++ Programmers/Lv. 0/모스부호 (1).cpp
@@ -0,0 +1,33 @@
+﻿/*
+* @title    : 모스부호 (1)(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230111
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string letter) {
+    string answer = "";
+    auto begin = letter.begin();
+    vector<string> morse = { ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.." };
+
+    for (auto iter = letter.begin(); ; ++iter) {
+        if (iter == letter.end()) {
+            string alphabet(begin, iter);
+            answer += find(morse.begin(), morse.end(), alphabet) - morse.begin() + 97;
+            break;
+        }
+
+        if (*iter == ' ') {
+            string alphabet(begin, iter);
+            answer += find(morse.begin(), morse.end(), alphabet) - morse.begin() + 97;
+            begin = iter + 1;
+        }
+    }
+    return answer;
+}
