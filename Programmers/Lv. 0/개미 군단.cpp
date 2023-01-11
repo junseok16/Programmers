6efc3f4f589ece938db23132be5225c1e97b4a8e
Junseok Tak <tjsuk0220@gmail.com>
2023-01-11 19:25:00 +0900

    Init: Create 개미 군단.cpp

---
+++ Programmers/Lv. 0/개미 군단.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* @title    : 개미 군단(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230111
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    answer += hp / 5;
+    hp %= 5;
+    answer += hp / 3;
+    hp %= 3;
+    answer += hp;
+    return answer;
+}
