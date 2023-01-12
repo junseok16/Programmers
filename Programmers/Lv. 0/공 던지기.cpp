e8f94d3b9c9ae3c8445ca0ded68d741ec5982bb5
Junseok Tak <tjsuk0220@gmail.com>
2023-01-12 20:15:37 +0900

    Init: Create 공 던지기.cpp

---
+++ Programmers/Lv. 0/공 던지기.cpp
@@ -0,0 +1,30 @@
+﻿/*
+* @title    : 공 던지기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230112
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> numbers, int k) {
+    int answer = 0;
+
+    auto iter = numbers.begin();
+    for (int i = 1; i < k; ++i) {
+        ++iter;
+        if (iter == numbers.end()) {
+            iter = iter = numbers.begin();
+        }
+        ++iter;
+        if (iter == numbers.end()) {
+            iter = iter = numbers.begin();
+        }
+    }
+
+    answer = *iter;
+    return answer;
+}
