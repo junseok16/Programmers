03697c350160491b6d37475b7aae2daa924a80d6
Junseok Tak <tjsuk0220@gmail.com>
2023-01-13 22:58:32 +0900

    Init: Create 합성수 찾기.cpp

---
+++ Programmers/Lv. 0/합성수 찾기.cpp
@@ -0,0 +1,34 @@
+﻿/*
+* @title    : 합성수 찾기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230113
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+bool isComposite(int num) {
+    if (num == 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (isComposite(i) == true) {
+            ++answer;
+        }
+    }
+    return answer;
+}
