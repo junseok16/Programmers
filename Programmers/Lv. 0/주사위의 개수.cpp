52178e5b48f9cc99b525e9cd91a7f9497a53748f
Junseok Tak <tjsuk0220@gmail.com>
2023-01-13 22:58:00 +0900

    Init: Create 주사위의 개수.cpp

---
+++ Programmers/Lv. 0/주사위의 개수.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* @title    : 주사위의 개수(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230113
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+    return answer;
+}
