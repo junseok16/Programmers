49cdf5cf4ed1323c09c268a020a02000c34d213c
Junseok Tak <tjsuk0220@gmail.com>
2023-01-13 22:58:12 +0900

    Init: Create 최댓값 만들기 (1).cpp

---
+++ Programmers/Lv. 0/최댓값 만들기 (1).cpp
@@ -0,0 +1,18 @@
+﻿/*
+* @title    : 최댓값 만들기 (1)(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230113
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end());
+    int answer = numbers[numbers.size() - 1] * numbers[numbers.size() - 2];
+    return answer;
+}
