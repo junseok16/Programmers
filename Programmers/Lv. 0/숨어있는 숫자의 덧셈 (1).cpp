bd57868c899e42b6ebb9aa58ec717bc2d21f73f2
Junseok Tak <tjsuk0220@gmail.com>
2023-01-15 23:41:41 +0900

    Init: Create 숨어있는 숫자의 덧셈 (1).cpp

---
+++ Programmers/Lv. 0/숨어있는 숫자의 덧셈 (1).cpp
@@ -0,0 +1,22 @@
+﻿/*
+* @title    : 숨어있는 숫자의 덧셈 (1)(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230115
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    for (const auto& e : my_string) {
+        if (1 <= e - '0' && e - '0' <= 9) {
+            answer += e - '0';
+        }
+    }
+    return answer;
+}
