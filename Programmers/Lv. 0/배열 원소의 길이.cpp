29e0786da76f6aeb2aa17d546da6d5ce8d480468
Junseok Tak <tjsuk0220@gmail.com>
2023-01-16 13:36:18 +0900

    Init: Create 배열 원소의 길이.cpp

---
+++ Programmers/Lv. 0/배열 원소의 길이.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : 배열 원소의 길이(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230116
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    for (const auto& e : strlist) {
+        answer.push_back(e.size());
+    }
+    return answer;
+}
