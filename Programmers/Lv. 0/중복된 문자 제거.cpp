492ea77ca6714798e75b69ad5fad8642c48af369
Junseok Tak <tjsuk0220@gmail.com>
2023-01-18 23:46:36 +0900

    Init: Create 중복된 문자 제거.cpp

---
+++ Programmers/Lv. 0/중복된 문자 제거.cpp
@@ -0,0 +1,28 @@
+﻿/*
+* @title    : 중복된 문자 제거(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230118
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string my_string) {
+    vector<int> sentence;
+
+    for (unsigned int i = 0; i < my_string.size(); ++i) {
+        if (find(sentence.begin(), sentence.end(), my_string[i]) == sentence.end()) {
+            sentence.push_back(my_string[i]);
+        }
+    }
+
+    string answer = "";
+    for (const auto& e : sentence) {
+        answer += e;
+    }
+    return answer;
+}
