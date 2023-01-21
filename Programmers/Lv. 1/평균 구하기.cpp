6b6cada7016bdeaad7494cc9d20b3d0f665a558c
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 21:25:10 +0900

    Init: Create 평균 구하기.cpp

---
+++ Programmers/Lv. 1/평균 구하기.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* @title    : 평균 구하기(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+
+    for (const auto& e : arr) {
+        answer += e;
+    }
+    answer /= arr.size();
+    return answer;
+}
