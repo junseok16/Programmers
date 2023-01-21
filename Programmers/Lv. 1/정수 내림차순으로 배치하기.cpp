356f4e46fb173b41693dd07335376691c12a2d13
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 22:00:39 +0900

    Init: Create 정수 내림차순으로 배치하기.cpp

---
+++ Programmers/Lv. 1/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,18 @@
+﻿/*
+* @title    : 정수 내림차순으로 배치하기(Lv. 1)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230121
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(long long n) {
+    string s = to_string(n);
+    sort(s.begin(), s.end(), greater<char>());
+    return stoll(s);
+}
