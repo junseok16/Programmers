06b1f88f075e7697aca901bab205ad181234c395
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 21:41:11 +0900

    Init: Create x만큼 간격이 있는 n개의 숫자.cpp

---
+++ Programmers/Lv. 1/x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* @title    : x만큼 간격이 있는 n개의 숫자(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230121
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    for (int i = x, j = 0; j < n; i += x, ++j) {
+        answer.push_back(i);
+    }
+    return answer;
+}
