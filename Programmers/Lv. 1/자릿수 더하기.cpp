fc4689fcf9781b4d3c9d30b11db872995e89af36
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 21:32:44 +0900

    Init: Create 자릿수 더하기.cpp

---
+++ Programmers/Lv. 1/자릿수 더하기.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* @title    : 자릿수 더하기(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <iostream>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    while (n > 0) {
+        answer += n % 10;
+        n /= 10;
+    }
+    return answer;
+}
