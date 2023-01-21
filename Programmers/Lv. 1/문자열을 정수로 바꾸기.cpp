3784a73f2edc61458fc3c8b67a425f11b1bb3e4f
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 21:41:43 +0900

    Init: Create 문자열을 정수로 바꾸기.cpp

---
+++ Programmers/Lv. 1/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* @title    : 문자열을 정수로 바꾸기(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230119
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s) {
+    int answer = stoi(s);
+    return answer;
+}
