ce5ca6d29549533522a7e7334a20588c8383c922
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 22:00:24 +0900

    Init: Create 하샤드 수.cpp

---
+++ Programmers/Lv. 1/하샤드 수.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* @title    : 하샤드 수(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230121
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int digit = 0, n = x;
+
+    while (x) {
+        digit += x % 10;
+        x /= 10;
+    }
+
+    if (n % digit == 0) { answer = true; }
+    else { answer = false; }
+    return answer;
+}
