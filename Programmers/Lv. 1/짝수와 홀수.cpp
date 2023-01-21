0e415762224ae13e7c2d1511a7653917027741bd
Junseok Tak <tjsuk0220@gmail.com>
2023-01-21 21:25:22 +0900

    Init: Create 짝수와 홀수.cpp

---
+++ Programmers/Lv. 1/짝수와 홀수.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* @title    : 짝수와 홀수(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230103
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+
+    if (num % 2) { answer = "Odd"; }
+    else { answer = "Even"; }
+    return answer;
+}
