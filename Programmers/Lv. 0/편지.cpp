d2af1d30d161745ca339c7e41b85e93cdc837bba
Junseok Tak <tjsuk0220@gmail.com>
2023-01-22 22:54:46 +0900

    Init: Create 편지.cpp

---
+++ Programmers/Lv. 0/편지.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* @title    : 편지(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230121
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string message) {
+    return message.size() * 2;
+}
