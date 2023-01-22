1c5074dd562651db465bce847635f3ab4e0177d2
Junseok Tak <tjsuk0220@gmail.com>
2023-01-22 23:18:24 +0900

    Init: Create 두 정수 사이의 합.cpp

---
+++ Programmers/Lv. 1/두 정수 사이의 합.cpp
@@ -0,0 +1,23 @@
+﻿/*
+* @title    : 두 정수 사이의 합(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230122
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int minNumber = min(a, b);
+    int maxNumber = max(a, b);
+
+    for (int i = minNumber; i <= maxNumber; ++i) {
+        answer += i;
+    }
+    return answer;
+}
