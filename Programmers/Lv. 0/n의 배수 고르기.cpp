68e5dcc71f8fad578d7e3394770b51989f1e2eab
Junseok Tak <tjsuk0220@gmail.com>
2023-01-23 21:35:52 +0900

    Init: Create n의 배수 고르기.cpp

---
+++ Programmers/Lv. 0/n의 배수 고르기.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* @title    : n의 배수 고르기(Lv. 0)
+* @content  : 코딩테스트 입문
+* @author   : 탁준석
+* @date     : 230122
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, vector<int> numlist) {
+    vector<int> answer;
+    for (const auto& e : numlist) {
+        if (e % n == 0) {
+            answer.push_back(e);
+        }
+    }
+    return answer;
+}
