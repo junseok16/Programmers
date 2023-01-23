e98d5e7bc96192064557a5160027fa6669e3a66f
Junseok Tak <tjsuk0220@gmail.com>
2023-01-23 21:26:15 +0900

    Init: Create 문자열 다루기 기본.cpp

---
+++ Programmers/Lv. 1/문자열 다루기 기본.cpp
@@ -0,0 +1,24 @@
+﻿/*
+* @title    : 문자열 다루기 기본(Lv. 1)
+* @content  : 연습문제
+* @author   : 탁준석
+* @date     : 230123
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(string s) {
+    if (s.size() != 4 && s.size() != 6) {
+        return false;
+    }
+
+    for (const auto& e : s) {
+        if (e - '0' < 0 || 9 < e - '0') {
+            return false;
+        }
+    }
+    return true;
+}
