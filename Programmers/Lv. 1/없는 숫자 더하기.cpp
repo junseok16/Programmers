873b34e274dcdb21cb44120af887eb41e7b57500
Junseok Tak <tjsuk0220@gmail.com>
2023-01-24 21:25:21 +0900

    Init: Create 없는 숫자 더하기.cpp

---
+++ Programmers/Lv. 1/없는 숫자 더하기.cpp
@@ -0,0 +1,26 @@
+﻿/*
+* @title    : 없는 숫자 더하기(Lv. 1)
+* @content  : 월간 코드 챌린지 시즌3
+* @author   : 탁준석
+* @date     : 230124
+* @state    : 완료
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    vector<int> isNumber = { 0, 1, 2, 3, 4, 5 ,6, 7, 8, 9 };
+
+    for (const auto& e : numbers) {
+        isNumber[e] = -1;
+    }
+    for (const auto& e : isNumber) {
+        if (e > 0) {
+            answer += e;
+        }
+    }
+    return answer;
+}
