96ee1e8206a89020575da4fa4dc699375162243b
Junseok Tak <tjsuk0220@gmail.com>
2023-05-03 20:11:04 +0900

    Feat: Refactor JadenCase 문자열 만들기.cpp

--- Programmers/Lv. 2/JadenCase 문자열 만들기.cpp
+++ Programmers/Lv. 2/JadenCase 문자열 만들기.cpp
@@ -2,43 +2,21 @@
 * @title    : JadenCase 문자열 만들기(Lv. 2)
 * @content  : 연습문제
 * @author   : 탁준석
-* @date     : 230501
+* @date     : 230503
 * @state    : 완료
 */
 
 #include <string>
 #include <vector>
-#include <sstream>
-#include <iostream>
 using namespace std;
 
 string solution(string s) {
-    stringstream ss(s);
-    string temp;
     string answer = "";
+    answer += toupper(s[0]);
 
-    while (getline(ss, temp, ' ')) {
-        if (temp.empty()) {
-            answer += " ";
-            continue;
-        }
-
-        // 첫 번째 문자가 소문자인 경우
-        if ('a' <= temp[0] && temp[0] <= 'z') {
-            temp[0] += 'A' - 'a';
-        }
-        // 그외 문자가 대문자인 경우
-        for (unsigned int i = 1; i < temp.size(); ++i) {
-            if ('A' <= temp[i] && temp[i] <= 'Z') {
-                temp[i] += 'a' - 'A';
-            }
-        }
-
-        answer += (temp + " ");
-    }
-
-    if (s.back() != ' ') {
-        answer.erase(answer.end() - 1);
+    // 첫 문자를 대문자로 변경하거나 나머지 문자를 소문자로 변경합니다.
+    for (unsigned int i = 1; i < s.size(); ++i) {
+        s[i - 1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
     }
     return answer;
 }
