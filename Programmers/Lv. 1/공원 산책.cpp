e0057d8564d842831a7032badc39796b58eca0f0
Junseok Tak <tjsuk0220@gmail.com>
2023-05-06 21:02:15 +0900

    Feat: Refactor 공원 산책.cpp

--- Programmers/Lv. 1/공원 산책.cpp
+++ Programmers/Lv. 1/공원 산책.cpp
@@ -8,103 +8,53 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
-#define X first
-#define Y second
+#include <unordered_map>
 using namespace std;
 
-vector<vector<char>> adj;
-pair<int, int> S, R;
-int N, M;
-
-void MakeGraph(vector<string>& park) {
-    N = park.size();
-    M = park[0].size();
-    S = { 0, 0 }, R = { 0, 0 };
-
-    adj = vector<vector<char>>(N, vector<char>(M, 'O'));
-    for (unsigned int i = 0; i < park.size(); ++i) {
-        for (unsigned int j = 0; j < park[i].size(); ++j) {
-            adj[i][j] = park[i][j];
-            if (adj[i][j] == 'S') { R = S = { i, j }; }
-        }
-    }
-}
-
-bool isObstacle(char op, int n) {
-    bool isObstacle = false;
-
-    switch (op) {
-    case 'E':
-        if (R.Y + n >= M) { isObstacle = true; break; }
-        for (int i = R.Y + 1; i <= R.Y + n; ++i) {
-            if (adj[R.X][i] == 'X') {
-                isObstacle = true;
-                break;
-            }
-        }
-        break;
-
-    case 'W':
-        if (R.Y - n < 0) { isObstacle = true; break; }
-        for (int i = R.Y - 1; i >= R.Y - n; --i) {
-            if (adj[R.X][i] == 'X') {
-                isObstacle = true;
-                break;
-            }
-        }
-        break;
-
-    case 'S':
-        if (R.X + n >= N) { isObstacle = true; break; }
-        for (int i = R.X + 1; i <= R.X + n; ++i) {
-            if (adj[i][R.Y] == 'X') {
-                isObstacle = true;
-                break;
-            }
-        }
-        break;
-
-    case 'N':
-        if (R.X - n < 0) { isObstacle = true; break; }
-        for (int i = R.X - 1; i >= R.X - n; --i) {
-            if (adj[i][R.Y] == 'X') {
-                isObstacle = true;
-                break;
-            }
-        }
-        break;
-    }
-    return isObstacle;
-}
+vector<int> dx = { 1, -1, 0, 0 };
+vector<int> dy = { 0, 0, 1, -1 };
+unordered_map<char, int> direction = { { 'S', 0 }, { 'N', 1 }, { 'E', 2 }, { 'W', 3 } };
 
 vector<int> solution(vector<string> park, vector<string> routes) {
-    MakeGraph(park);
+    int H = park.size();
+    int W = park[0].size();
+    int cx, cy;
 
-    for (unsigned int i = 0; i < routes.size(); ++i) {
-        char op = routes[i][0];
-        int n = routes[i][2] - '0';
-
-        if (isObstacle(op, n)) {
-            continue;
-        }
-
-        switch (op) {
-        case 'E':
-            R = { R.X, R.Y + n };
-            break;
-        case 'W':
-            R = { R.X, R.Y - n };
-            break;
-        case 'S':
-            R = { R.X + n, R.Y };
-            break;
-        case 'N':
-            R = { R.X - n, R.Y };
-            break;
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            if (park[i][j] == 'S') {
+                cx = i, cy = j;
+            }
         }
     }
 
-    vector<int> answer{ R.X, R.Y };
-    return answer;
+    int nx, ny;
+    for (unsigned int i = 0; i < routes.size(); ++i) {
+        int op = direction[routes[i][0]];
+        int n = routes[i][2] - '0';
+        bool isValid = true;
+
+        nx = cx; ny = cy;
+        for (int j = 0; j < n; ++j) {
+            nx += dx[op];
+            ny += dy[op];
+
+            // 공원을 벗어나는 경우
+            if (nx < 0 || ny < 0 || nx >= H || ny >= W) {
+                isValid = false;
+                break;
+            }
+            // 장애물을 만나는 경우
+            if (park[nx][ny] == 'X') {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            cx = nx; cy = ny;
+        }
+    }
+
+    return { cx, cy };
 }
